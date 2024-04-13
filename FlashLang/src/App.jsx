9be65e40c354cb8c06b7cbc622b0053b3b2b377c
NayeemK14628 <153224838+NayeemK14628@users.noreply.github.com>
2024-04-13 14:21:55 -0400

    Update App.jsx

--- FlashLang/src/App.jsx
+++ FlashLang/src/App.jsx
@@ -2,13 +2,56 @@ import { motion } from "framer-motion"
 
 const App = () => {
   return (
-    <div className="flex justify-center items-center h-screen overflow-x-hidden">
+    <div className="flex justify-center mt-20 h-screen overflow-x-hidden">
       <section >
+
+        <div className="text-center mb-20">
+        <motion.h1 
+        initial={{scale: 0 }}
+        animate={{scale: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 20,
+          delay: 0.25
+        }}
+        whileHover={{scale: 1.1}} className="text-6xl text-green-600">
+          Welcome to FlashLang!
+        </motion.h1>
+        </div>
+
+        <div className="text-center">
         <motion.button whileTap={{scale: 0.9}}
-         whileHover={{scale: 1.1, backgroundColor: "white", color: "black"}}
-         className="bg-green-400 aspect-rectangle w-40 h-20 rounded-lg text-2xl text-white-100 font-light tracking-wide">
-          Welcome
+         initial={{ scale: 0 }}
+        animate={{scale: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 20,
+          delay: 0.50
+        }}
+        whileHover={{scale: 1.1, color: "black"}}
+         className="bg-green-600 w-45 h-20 px-3 rounded-full text-4xl text-white-100 font-bold tracking-tight">
+          Sign In With Google
         </motion.button>
+
+        <h1> </h1>
+
+        <motion.button whileTap={{scale: 0.9}}
+        initial={{scale: 0 }}
+        animate={{scale: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 20,
+          delay: 0.75
+        }}
+         whileHover={{scale: 1.1}}
+         className="bg-slate-950 w-30 h-10 px-3 mt-10 rounded-full text-xl text-green-600 font-bold tracking-tight">
+          New User?
+        </motion.button>
+        </div>
+
       </section>
     </div>
   )
